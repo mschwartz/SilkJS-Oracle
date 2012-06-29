b83a33e84f2541ec09dedced860514d90650d581
Michael Schwartz <mschwartz@pint.com>
2012-06-29 07:57:28 -0700

    Driver, in progress.

--- src/oracle.cpp
+++ src/oracle.cpp
@@ -8,7 +8,7 @@ struct ostate {
     Connection *con;
 };
 
-static JSVAL oracle_connect(JSARGS args) {
+static JSVAL connect(JSARGS args) {
     String::Utf8Value host(args[0]->ToString());
     String::Utf8Value user(args[1]->ToString());
     String::Utf8Value password(args[2]->ToString());
@@ -20,11 +20,63 @@ static JSVAL oracle_connect(JSARGS args) {
     return External::New(o);
 }
 
-static JSVAL oracle_close(JSARGS args) {
+static JSVAL close(JSARGS args) {
     ostate *o = (ostate *)JSEXTERN(args[0]);
     o->environment->terminateConnection(o->con);
+	Environment::terminateEnvironment(o->env);
     delete o;
     return Undefined();
+}
+
+static JSVAL getDataRow(JSARGS args) {
+	ostate *o = (ostate *)JSEXTERN(args[0]);
+	String::Utf8Value query(args[1]->ToString());
+	Statement *s = NULL;
+	ResultSet *r = NULL;
+
+	try {
+		s = o->con->createStatement(*query);
+		r = s->executeQuery();
+		vector<MetaData>cols = r->getColumnListMetaData();
+		int num_fields = cols.size();
+		Local<String> names[num_fields];
+		int types[num_fields];
+		for (int n=0; n<num_fields; n++) {
+			names[n] = String::New(cols[n].getString(MetaData::ATTR_NAME));
+			types[n] = cols[n].getInt(MetaData::ATTR_DATA_TYPE);
+		}
+		if (!r->next()) {
+			s->closeResultSet(r);
+			o->con->terminateStatement(s);
+			return False();
+		}
+		JSOBJ obj = Object::New();
+		for (int i=0; i<num_fields; i++) {
+			if (r->isNull(i)) {
+				o->Set(names[i], Null());
+			}
+			else {
+				switch (types[i]) {
+					case SQL_INT:
+						o->Set(names[i], Number::New(r->getInt(i)));
+						break;
+
+				}
+			}
+		}
+		s->closeResultSet(r);
+		o->con->terminateStatement(s);
+		return obj;
+	}
+	catch (SQLException &e) {
+		if (r) {
+			s->closeResultSet(r);
+		}
+		if (s) {
+			o->con->terminateStatement(s);
+		}
+		return ThrowException(String::New(e.what()));
+	}
 }
 
 /*
